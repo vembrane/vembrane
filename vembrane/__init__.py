b6e74f64696218d880b8d3aee93a98cb81e98183
Till Hartmann <till.hartmann@udo.edu>
2020-07-03 14:41:38 +0200

    check for syntax error

--- vembrane/__init__.py
+++ vembrane/__init__.py
@@ -93,12 +93,17 @@ def filter_vcf(
 def check_filter_expression(expression: str,) -> str:
     if ".__" in expression:
         raise InvalidExpression(expression, "The expression must not contain '.__'")
-    tree = ast.parse(expression, mode="eval")
-    if isinstance(tree.body, (ast.BoolOp, ast.Compare)):
-        return expression
-    else:
-        # TODO possibly check for ast.Call, func return type
-        return expression
+    try:
+        tree = ast.parse(expression, mode="eval")
+        if isinstance(tree.body, (ast.BoolOp, ast.Compare)):
+            return expression
+        else:
+            # TODO possibly check for ast.Call, func return type
+            return expression
+    except SyntaxError:
+        raise InvalidExpression(
+            expression, f"The expression has to be syntactically correct."
+        )
 
 
 def main():
