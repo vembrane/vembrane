9043101adc5a968af630e8eb435d88f456caccd0
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2020-07-09 20:37:43 +0200

    reformat

--- vembrane/__init__.py
+++ vembrane/__init__.py
@@ -47,15 +47,20 @@ class Map(dict):
         if kwargs:
             for k, v in kwargs.iteritems():
                 self[k] = v
+
     def __getattr__(self, attr):
         return self.get(attr)
+
     def __setattr__(self, key, value):
         self.__setitem__(key, value)
+
     def __setitem__(self, key, value):
         super(Map, self).__setitem__(key, value)
         self.__dict__.update({key: value})
+
     def __delattr__(self, item):
         self.__delitem__(item)
+
     def __delitem__(self, key):
         super(Map, self).__delitem__(key)
         del self.__dict__[key]
