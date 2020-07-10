b1ab203292ce2f76fcd39ef069d3e3fe5f0fa7d2
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2020-07-10 10:47:24 +0200

    Update __init__.py
    remove map

--- vembrane/__init__.py
+++ vembrane/__init__.py
@@ -36,36 +36,6 @@ globals_whitelist = {
     **{name: mod for name, mod in vars(math).items() if not name.startswith("__")},
 }
 
-
-class Map(dict):
-    def __init__(self, *args, **kwargs):
-        super(Map, self).__init__(*args, **kwargs)
-        for arg in args:
-            if isinstance(arg, dict):
-                for k, v in arg.iteritems():
-                    self[k] = v
-        if kwargs:
-            for k, v in kwargs.iteritems():
-                self[k] = v
-
-    def __getattr__(self, attr):
-        return self.get(attr)
-
-    def __setattr__(self, key, value):
-        self.__setitem__(key, value)
-
-    def __setitem__(self, key, value):
-        super(Map, self).__setitem__(key, value)
-        self.__dict__.update({key: value})
-
-    def __delattr__(self, item):
-        self.__delitem__(item)
-
-    def __delitem__(self, key):
-        super(Map, self).__delitem__(key)
-        del self.__dict__[key]
-
-
 class Sample:
     def __init__(self, record_idx: int, sample: str, format_data: Dict[str, Any]):
         self._record_idx = record_idx
