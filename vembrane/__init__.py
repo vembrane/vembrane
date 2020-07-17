bdab283bc5b3a74d2494c989359e6aaef42b4210
Till Hartmann <till.hartmann@udo.edu>
2020-07-17 11:59:51 +0200

    wip

--- vembrane/__init__.py
+++ vembrane/__init__.py
@@ -210,11 +210,155 @@ class Expression:
         return self.__str__()
 
 
+def dump(node, annotate_fields=True, include_attributes=False, indent="  "):
+    """
+    Return a formatted dump of the tree in *node*.  This is mainly useful for
+    debugging purposes.  The returned string will show the names and the values
+    for fields.  This makes the code impossible to evaluate, so if evaluation is
+    wanted *annotate_fields* must be set to False.  Attributes such as line
+    numbers and column offsets are not dumped by default.  If this is wanted,
+    *include_attributes* can be set to True.
+    """
+
+    def _format(node, level=0):
+        if isinstance(node, ast.AST):
+            fields = [(a, _format(b, level)) for a, b in ast.iter_fields(node)]
+            if include_attributes and node._attributes:
+                fields.extend(
+                    [(a, _format(getattr(node, a), level)) for a in node._attributes]
+                )
+            return "".join(
+                [
+                    node.__class__.__name__,
+                    "(",
+                    ", ".join(
+                        ("%s=%s" % field for field in fields)
+                        if annotate_fields
+                        else (b for a, b in fields)
+                    ),
+                    ")",
+                ]
+            )
+        elif isinstance(node, list):
+            lines = ["["]
+            lines.extend(
+                (indent * (level + 2) + _format(x, level + 2) + "," for x in node)
+            )
+            if len(lines) > 1:
+                lines.append(indent * (level + 1) + "]")
+            else:
+                lines[-1] += "]"
+            return "\n".join(lines)
+        return repr(node)
+
+    if not isinstance(node, ast.AST):
+        raise TypeError("expected AST, got %r" % node.__class__.__name__)
+    return _format(node)
+
+
+class NoopList(list):
+    def append(self, item):
+        pass
+
+    def clear(self, *args, **kwargs):
+        pass
+
+    def extend(self, *args, **kwargs):
+        pass
+
+    def index(self, *args, **kwargs):
+        return self
+
+    def insert(self, *args, **kwargs):
+        pass
+
+    def pop(self, *args, **kwargs):
+        pass
+
+    def remove(self, *args, **kwargs):
+        pass
+
+    def reverse(self, *args, **kwargs):
+        pass
+
+    def sort(self, *args, **kwargs):
+        pass
+
+    def __add__(self, *args, **kwargs):
+        pass
+
+    def __contains__(self, *args, **kwargs):
+        return False
+
+    def __delitem__(self, *args, **kwargs):
+        pass
+
+
+ALL = NoopList()
+
+
+class FieldLister(ast.NodeVisitor):
+    constants = set()
+
+    def __init__(self):
+        self.data = defaultdict(list)
+
+    def visit(self, root: ast.AST):
+        for node in ast.walk(root):
+            for child in ast.iter_child_nodes(node):
+                child.parent = node
+        super().visit(root)
+
+    def visit_Subscript(self, node: ast.Subscript) -> Any:
+        if isinstance(node.value, ast.Name) and isinstance(node.slice, ast.Index):
+            print("Subscript+Name: ", node.value.id)
+            field = node.value.id
+            key = node.slice.value
+            if isinstance(key, (ast.Str, ast.Constant)):
+                # python < 3.8
+                if isinstance(key, ast.Str):
+                    self.data[field].append(key.s)
+                # python >= 3.8
+                elif isinstance(key, ast.Constant):
+                    self.data[field].append(key.value)
+            else:
+                self.data[field] = ALL
+        else:
+            print("Subscript: ", node.value)
+        self.generic_visit(node)
+
+    def visit_Constant(self, node: ast.Constant) -> Any:
+        print("Constant: ", node.value)
+        self.constants.add(node.value)
+        self.generic_visit(node)
+
+    def visit_Str(self, node: ast.Str) -> Any:
+        print("Str: ", node.s)
+        self.constants.add(node.s)
+        self.generic_visit(node)
+
+    def visit_Name(self, node: ast.Name) -> Any:
+        print("Name: ", node.id)
+        self.constants.add(node.id)
+        self.generic_visit(node)
+
+    def visit_Dict(self, node: ast.Dict) -> Any:
+        print("Dict: ", node)
+        self.generic_visit(node)
+
+    def get_info(self):
+        return self.constants, self.data
+
+
 class Environment:
     def __init__(self, expression: Expression, vcf_header: VariantHeader):
         # We only wish to access (and in the case of ANN, parse) the fields that
         # are part of the expression. To do that, we build the AST for the expression…
         tree = ast.parse(expression.raw_expression)
+        print(dump(tree))
+        field_lister = FieldLister()
+        field_lister.visit(tree)
+        print(field_lister.get_info())
 
         # … and get all nodes that correspond to fields/symbols/:
         # `names` contains *all* nodes that refer to a Constant/Name/Str/…
@@ -228,6 +372,8 @@ class Environment:
             )
             | set(node.s for node in ast.walk(tree) if isinstance(node, ast.Str))
         )
+        print(names)
+        exit(1)
 
         # Restrict names/strings/identifiers/functions/symbols to the ones actually
         # seen in the expression. We use `names` here for everything, even though we
@@ -490,7 +636,7 @@ def main():
             ),
         )
 
-        with VariantFile(args.output, "w" + fmt, header=header,) as out:
+        with VariantFile(args.output, "w" + fmt, header=None,) as out:
             records = filter_vcf(vcf, expression, keep_unmatched=args.keep_unmatched,)
             if args.statistics is not None:
                 records = statistics(records, vcf, args.statistics, args.annotation_key)
