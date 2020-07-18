74c698ad0caa9e1b5bc2a3f67f9b22ba92bcd1a1
Till Hartmann <till.hartmann@udo.edu>
2020-07-18 11:00:29 +0200

    simplify ast walking

--- vembrane/__init__.py
+++ vembrane/__init__.py
@@ -1,3 +1,5 @@
+from vembrane.aux import FieldLister
+
 try:
     from importlib.metadata import version, PackageNotFoundError  # type: ignore
 except ImportError:  # pragma: no cover
@@ -15,7 +17,7 @@ import sys
 from collections import defaultdict
 from functools import lru_cache
 from sys import stderr
-from typing import Iterator, List, Dict, Any, Set, Tuple, Iterable
+from typing import Iterator, List, Dict, Any, Set, Tuple
 
 import yaml
 from pysam import VariantFile, VariantRecord, VariantHeader
@@ -210,154 +212,6 @@ class Expression:
         return self.__str__()
 
 
-def dump(node, annotate_fields=True, include_attributes=False, indent="  "):
-    """
-    Return a formatted dump of the tree in *node*.  This is mainly useful for
-    debugging purposes.  The returned string will show the names and the values
-    for fields.  This makes the code impossible to evaluate, so if evaluation is
-    wanted *annotate_fields* must be set to False.  Attributes such as line
-    numbers and column offsets are not dumped by default.  If this is wanted,
-    *include_attributes* can be set to True.
-    """
-
-    def _format(node, level=0):
-        if isinstance(node, ast.AST):
-            fields = [(a, _format(b, level)) for a, b in ast.iter_fields(node)]
-            if include_attributes and node._attributes:
-                fields.extend(
-                    [(a, _format(getattr(node, a), level)) for a in node._attributes]
-                )
-            return "".join(
-                [
-                    node.__class__.__name__,
-                    "(",
-                    ", ".join(
-                        ("%s=%s" % field for field in fields)
-                        if annotate_fields
-                        else (b for a, b in fields)
-                    ),
-                    ")",
-                ]
-            )
-        elif isinstance(node, list):
-            lines = ["["]
-            lines.extend(
-                (indent * (level + 2) + _format(x, level + 2) + "," for x in node)
-            )
-            if len(lines) > 1:
-                lines.append(indent * (level + 1) + "]")
-            else:
-                lines[-1] += "]"
-            return "\n".join(lines)
-        return repr(node)
-
-    if not isinstance(node, ast.AST):
-        raise TypeError("expected AST, got %r" % node.__class__.__name__)
-    return _format(node)
-
-
-class NoopSet(set):
-    def add(self, item):
-        pass
-
-    def clear(self, *args, **kwargs):
-        pass
-
-    def pop(self, *args, **kwargs):
-        pass
-
-    def remove(self, *args, **kwargs):
-        pass
-
-    def __contains__(self, *args, **kwargs):
-        return True
-
-    def copy(self) -> "NoopSet":
-        return self
-
-    def intersection(self, *s: Iterable[Any]) -> Set[Any]:
-        return set(s)
-
-    def __rand__(self, other) -> Set[Any]:
-        return other
-
-
-ALL = NoopSet()
-
-
-class FieldLister(ast.NodeVisitor):
-    def __init__(self):
-        self.field_accesses = defaultdict(set)
-        self.modules = set()
-        self.functions = set()
-        self.symbols = set()
-
-    def visit(self, root: ast.AST):
-        for node in ast.walk(root):
-            for child in ast.iter_child_nodes(node):
-                child.parent = node
-        super().visit(root)
-
-    def visit_Subscript(self, node: ast.Subscript) -> Any:
-        # node slice can be either Index, Slice or ExtSlice
-        # anything that isn't Index makes this expression "complex"
-        if isinstance(node.value, (ast.Name, ast.Attribute)) and isinstance(
-            node.slice, ast.Index
-        ):
-            field = node.value.id
-            key = node.slice.value
-            if isinstance(key, (ast.Str, ast.Constant)):
-                # python < 3.8
-                if isinstance(key, ast.Str):
-                    self.field_accesses[field].add(key.s)
-                # python >= 3.8
-                elif isinstance(key, ast.Constant):
-                    self.field_accesses[field].add(key.value)
-            else:
-                self.field_accesses[field] = ALL
-        self.generic_visit(node)
-
-    def visit_Attribute(self, node: ast.Attribute) -> Any:
-        self.modules.add(node.value.id)
-        self.generic_visit(node)
-
-    def visit_Call(self, node: ast.Call) -> Any:
-        func = node.func
-        if isinstance(func, ast.Name):
-            self.functions.add(func.id)
-        elif isinstance(func, ast.Attribute):
-            self.modules.add(func.value.id)
-            self.functions.add(func.value.id + "." + func.attr)
-        self.generic_visit(node)
-
-    def visit_Constant(self, node: ast.Constant) -> Any:
-        if not isinstance(node.parent, ast.Subscript):
-            # print("Constant: ", node.value)
-            # self.constants.add(node.value)
-            pass
-        self.generic_visit(node)
-
-    def visit_Str(self, node: ast.Str) -> Any:
-        if not isinstance(node.parent, ast.Subscript):
-            # print("Str: ", node.s)
-            # self.constants.add(node.s)
-            pass
-        self.generic_visit(node)
-
-    def visit_Name(self, node: ast.Name) -> Any:
-        self.symbols.add(node.id)
-        self.generic_visit(node)
-
-    def visit_Dict(self, node: ast.Dict) -> Any:
-        self.generic_visit(node)
-
-    def __str__(self):
-        return str(self.__dict__)
-
-    def __repr__(self):
-        return self.__str__()
-
-
 class Environment:
     def __init__(self, expression: Expression, vcf_header: VariantHeader):
         # We only wish to access (and in the case of ANN, parse) the fields that
@@ -393,11 +247,9 @@ class Environment:
             "REF",
             "ALT",
             "SAMPLES",
-        } & field_lister.symbols
+        } & field_lister.names
 
-        available_symbols = globals_whitelist.keys() & (
-            field_lister.modules | field_lister.symbols | field_lister.functions
-        )
+        available_symbols = globals_whitelist.keys() & field_lister.names
         self.globals_whitelist = {
             key: globals_whitelist[key] for key in available_symbols
         }
---
+++ vembrane/aux.py
@@ -0,0 +1,118 @@
+import ast
+from collections import defaultdict
+from typing import Iterable, Any, Set
+
+
+def dump(node, annotate_fields=True, include_attributes=False, indent="  "):
+    """
+    Return a formatted dump of the tree in *node*.  This is mainly useful for
+    debugging purposes.  The returned string will show the names and the values
+    for fields.  This makes the code impossible to evaluate, so if evaluation is
+    wanted *annotate_fields* must be set to False.  Attributes such as line
+    numbers and column offsets are not dumped by default.  If this is wanted,
+    *include_attributes* can be set to True.
+    """
+
+    def _format(node, level=0):
+        if isinstance(node, ast.AST):
+            fields = [(a, _format(b, level)) for a, b in ast.iter_fields(node)]
+            if include_attributes and node._attributes:
+                fields.extend(
+                    [(a, _format(getattr(node, a), level)) for a in node._attributes]
+                )
+            return "".join(
+                [
+                    node.__class__.__name__,
+                    "(",
+                    ", ".join(
+                        ("%s=%s" % field for field in fields)
+                        if annotate_fields
+                        else (b for a, b in fields)
+                    ),
+                    ")",
+                ]
+            )
+        elif isinstance(node, list):
+            lines = ["["]
+            lines.extend(
+                (indent * (level + 2) + _format(x, level + 2) + "," for x in node)
+            )
+            if len(lines) > 1:
+                lines.append(indent * (level + 1) + "]")
+            else:
+                lines[-1] += "]"
+            return "\n".join(lines)
+        return repr(node)
+
+    if not isinstance(node, ast.AST):
+        raise TypeError("expected AST, got %r" % node.__class__.__name__)
+    return _format(node)
+
+
+class NoopSet(set):
+    def add(self, item):
+        pass
+
+    def clear(self, *args, **kwargs):
+        pass
+
+    def pop(self, *args, **kwargs):
+        pass
+
+    def remove(self, *args, **kwargs):
+        pass
+
+    def __contains__(self, *args, **kwargs):
+        return True
+
+    def copy(self) -> "NoopSet":
+        return self
+
+    def intersection(self, *s: Iterable[Any]) -> Set[Any]:
+        return set(s)
+
+    def __rand__(self, other) -> Set[Any]:
+        return other
+
+
+ALL = NoopSet()
+
+
+class FieldLister(ast.NodeVisitor):
+    def __init__(self):
+        self.field_accesses = defaultdict(set)
+        self.names = set()
+
+    def visit(self, root: ast.AST):
+        for node in ast.walk(root):
+            if hasattr(node, "id"):
+                self.names.add(node.id)
+        super().visit(root)
+
+    def visit_Subscript(self, node: ast.Subscript) -> Any:
+        # expressions of the form "INFO['DP']" or "ANN['Annotation_Impact']".
+        # `node.slice` can be either Index, Slice or ExtSlice.
+        # Anything that isn't Index makes this expression "complex"
+        if isinstance(node.value, (ast.Name, ast.Attribute)) and isinstance(
+            node.slice, ast.Index
+        ):
+            field = node.value.id
+            key = node.slice.value
+            if isinstance(key, (ast.Str, ast.Constant)):
+                # python < 3.8
+                if isinstance(key, ast.Str):
+                    self.field_accesses[field].add(key.s)
+                # python >= 3.8
+                elif isinstance(key, ast.Constant):
+                    self.field_accesses[field].add(key.value)
+            else:
+                # if the Index value is not a Str/Constant,
+                # all keys of the field have to be available
+                self.field_accesses[field] = ALL
+        self.generic_visit(node)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def __repr__(self):
+        return self.__str__()
