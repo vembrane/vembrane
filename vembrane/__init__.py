61b927874c256f50db0bda9b2e5cf4373363d7c3
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2020-09-18 14:38:34 +0200

    classes to classes.py

--- vembrane/__init__.py
+++ vembrane/__init__.py
@@ -15,13 +15,11 @@ import yaml
 from pysam import VariantFile, VariantRecord, VariantHeader
 
 from vembrane.ann_types import NA, type_info, ANN_TYPER
-from vembrane.errors import (
-    UnknownAnnotation,
-    UnknownInfoField,
-    InvalidExpression,
-    UnknownFormatField,
-    UnknownSample,
-    VembraneError,
+from vembrane.classes import (
+    Sample,
+    Format,
+    Info,
+    Annotation,
 )
 
 globals_whitelist = {
@@ -39,55 +37,6 @@ globals_whitelist = {
     },
     **{name: mod for name, mod in vars(math).items() if not name.startswith("__")},
 }
-
-
-class Sample:
-    def __init__(self, record_idx: int, sample: str, format_data: Dict[str, Any]):
-        self._record_idx = record_idx
-        self._sample = sample
-        self._data = format_data
-
-    def __getitem__(self, item):
-        try:
-            return self._data[item]
-        except KeyError as ke:
-            raise UnknownFormatField(self._record_idx, self._sample, ke)
-
-
-class Format:
-    def __init__(self, record_idx: int, sample_formats: Dict[Sample, Dict[str, Any]]):
-        self._record_idx = record_idx
-        self._sample_formats = sample_formats
-
-    def __getitem__(self, item):
-        try:
-            return self._sample_formats[item]
-        except KeyError as ke:
-            raise UnknownSample(self._record_idx, ke)
-
-
-class Info:
-    def __init__(self, record_idx: int, info_dict: Dict[str, Dict[str, Any]]):
-        self._record_idx = record_idx
-        self._info_dict = info_dict
-
-    def __getitem__(self, item):
-        try:
-            return self._info_dict[item]
-        except KeyError as ke:
-            raise UnknownInfoField(self._record_idx, ke)
-
-
-class Annotation:
-    def __init__(self, record_idx: int, annotation_data: Dict[str, Any]):
-        self._record_idx = record_idx
-        self._data = annotation_data
-
-    def __getitem__(self, item):
-        try:
-            return self._data[item]
-        except KeyError as ke:
-            raise UnknownAnnotation(self._record_idx, ke)
 
 
 def get_annotation_keys(header: VariantHeader) -> List[str]:
