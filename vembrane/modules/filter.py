7a861a01c21e8b53030fa50ade72e09a58d22963
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2020-09-28 15:19:35 +0200

    Update vembrane/modules/filter.py
    Co-authored-by: Till Hartmann <till.hartmann@udo.edu>

--- vembrane/modules/filter.py
+++ vembrane/modules/filter.py
@@ -141,7 +141,7 @@ def filter_vcf(
 
             if is_bnd:
                 if event not in events:
-                    # only bnds with valid event
+                    # only bnds with a valid associated event need to be considered, so skip the rest
                     continue
             else:
                 if not preserve_order:
