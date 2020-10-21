969f5841517f36072ded2adbd2097461d2cb3cc1
Till Hartmann <till.hartmann@udo.edu>
2020-10-21 14:59:35 +0200

    clearer naming conventions, collapse testcases into one

--- vembrane/globals.py
+++ vembrane/globals.py
@@ -131,14 +131,26 @@ def custom_functions(env) -> Dict[str, Any]:
             env,
             {},
         ),
-        "count_ref": eval(
+        "count_any_ref": eval(
             "lambda: sum(any(x == 0 for x in FORMAT['GT'][s]) for s in SAMPLES)",
             env,
             {},
         ),
-        "count_var": eval(
+        "count_any_var": eval(
             "lambda: "
             "sum(any(x != 0 for x in FORMAT['GT'][s] if x is not NA) "
+            "for s in SAMPLES)",
+            env,
+            {},
+        ),
+        "count_hom_ref": eval(
+            "lambda: sum(all(x == 0 for x in FORMAT['GT'][s]) for s in SAMPLES)",
+            env,
+            {},
+        ),
+        "count_hom_var": eval(
+            "lambda: "
+            "sum(all(x != 0 for x in FORMAT['GT'][s] if x is not NA) "
             "for s in SAMPLES)",
             env,
             {},
@@ -158,11 +170,22 @@ def custom_functions(env) -> Dict[str, Any]:
             {},
         ),
         "is_ref": eval(
-            "lambda sample: any(x == 0 for x in FORMAT['GT'][sample])",
+            "lambda sample: all(x == 0 for x in FORMAT['GT'][sample])",
             env,
             {},
         ),
         "is_var": eval(
+            "lambda sample: "
+            "all(x != 0 for x in FORMAT['GT'][sample] if x is not NA)",
+            env,
+            {},
+        ),
+        "has_ref": eval(
+            "lambda sample: any(x == 0 for x in FORMAT['GT'][sample])",
+            env,
+            {},
+        ),
+        "has_var": eval(
             "lambda sample: "
             "any(x != 0 for x in FORMAT['GT'][sample] if x is not NA)",
             env,
