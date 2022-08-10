b31339aacf4d2965b33ddc1d893747c09844b427
Till Hartmann <till.hartmann@udo.edu>
2022-08-10 16:52:59 +0200

    make black and isort work together nicely

--- vembrane/__init__.py
+++ vembrane/__init__.py
@@ -1,7 +1,7 @@
 try:
-    from importlib.metadata import version, PackageNotFoundError  # type: ignore
+    from importlib.metadata import PackageNotFoundError, version  # type: ignore
 except ImportError:  # pragma: no cover
-    from importlib_metadata import version, PackageNotFoundError  # type: ignore
+    from importlib_metadata import PackageNotFoundError, version  # type: ignore
 try:
     __version__ = version(__name__)
 except PackageNotFoundError:  # pragma: no cover
