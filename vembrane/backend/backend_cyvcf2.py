3efc40790151edc06e5dddf1623029274c11be78
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2023-09-16 15:08:24 +0000

    some fixes for cyvcf, working tests

--- vembrane/backend/backend_cyvcf2.py
+++ vembrane/backend/backend_cyvcf2.py
@@ -14,93 +14,6 @@ from vembrane.backend.base import (
 )
 
 
-class Cyvcf2RecordInfo(VCFRecordInfo):
-    def __init__(
-        self,
-        record: Variant,
-    ):
-        self._record = record
-
-    def __getitem__(self, item):
-        return self._record.INFO[item]
-
-    def __setitem__(self, key, value):
-        self._record.INFO[key] = value
-
-    def __contains__(self, item):
-        return item in self._record.INFO
-
-    def get(self, item, default=None):
-        return self._record.INFO.get(item, default)
-
-
-class Cyvcf2RecordFilter(VCFRecordFilter):
-    def __init__(self, record: Variant):
-        self._record = record
-
-    def __iter__(self):
-        yield from self._record.FILTERS
-
-    def add(self, tag: str):
-        self._record.FILTER.add(tag)
-
-
-class Cyvcf2VCFRecord(VCFRecord):
-    def __init__(self, record: Variant):
-        self._record = record
-
-    @property
-    def contig(self) -> str:
-        return self._record.CHROM
-
-    @property
-    def position(self) -> int:
-        return self._record.POS
-
-    @property
-    def id(self) -> str:
-        return self._record.ID
-
-    @property
-    def reference_allele(self) -> str:
-        return self._record.REF
-
-    @property
-    def alt_alleles(self) -> Tuple[str]:
-        return tuple(self._record.ALT)
-
-    @property
-    def quality(self) -> float:
-        return self._record.QUAL
-
-    @property
-    def filter(self) -> VCFRecordFilter:
-        return Cyvcf2RecordFilter(self._record)
-
-    @property
-    def info(self) -> VCFRecordInfo:
-        return Cyvcf2RecordInfo(self._record)
-
-    @property
-    def format(self) -> VCFRecordFormat:
-        return Cyvcf2RecordInfo(self._record)
-
-    # @property
-    # def samples(self):
-    #     return self._record.samples
-
-    def __repr__(self):
-        return self._record.__repr__()
-
-    def __str__(self):
-        return self._record.__str__()
-
-    def __eq__(self, other):
-        return (
-            self._record.__repr__() == other._record.__repr__()
-        )  # TODO: implement real check for values
-
-
 class Cyvcf2VCFReader(VCFReader):
     def __init__(self, filename: str):
         self.filename = filename
@@ -112,7 +25,7 @@ class Cyvcf2VCFReader(VCFReader):
         return self
 
     def __next__(self):
-        return Cyvcf2VCFRecord(self._iter_file.__next__())
+        return Cyvcf2VCFRecord(self._iter_file.__next__(), self._header)
 
     # @property
     # def header(self):
@@ -167,19 +80,105 @@ class Cyvcf2VCFHeader(VCFHeader):
         return self._data_category["FILTER"]
 
 
+class Cyvcf2VCFRecord(VCFRecord):
+    def __init__(self, record: Variant, header: Cyvcf2VCFHeader):
+        self._record = record
+        self._header = header
+
+    @property
+    def contig(self) -> str:
+        return self._record.CHROM
+
+    @property
+    def position(self) -> int:
+        return self._record.POS
+
+    @property
+    def id(self) -> str:
+        return self._record.ID
+
+    @property
+    def reference_allele(self) -> str:
+        return self._record.REF
+
+    @property
+    def alt_alleles(self) -> Tuple[str]:
+        return tuple(self._record.ALT)
+
+    @property
+    def quality(self) -> float:
+        return self._record.QUAL
+
+    @property
+    def filter(self) -> VCFRecordFilter:
+        return Cyvcf2RecordFilter(self._record)
+
+    @property
+    def info(self) -> VCFRecordInfo:
+        return Cyvcf2RecordInfo(self._record, self._header)
+
+    @property
+    def format(self) -> VCFRecordFormat:
+        return Cyvcf2RecordInfo(self._record)
+
+    # @property
+    # def samples(self):
+    #     return self._record.samples
+
+    def __repr__(self):
+        return self._record.__repr__()
+
+    def __str__(self):
+        return self._record.__str__()
+
+    def __eq__(self, other):
+        return (
+            self._record.__repr__() == other._record.__repr__()
+        )  # TODO: implement real check for values
+
+
+class Cyvcf2RecordFilter(VCFRecordFilter):
+    def __init__(self, record: Variant):
+        self._record = record
+
+    def __iter__(self):
+        yield from self._record.FILTERS
+
+    def add(self, tag: str):
+        self._record.FILTERS.append(tag)
+
+
+class Cyvcf2RecordInfo(VCFRecordInfo):
+    def __init__(
+        self,
+        record: Variant,
+        header: Cyvcf2VCFHeader,
+    ):
+        self._record = record
+        self._header = header
+
+    def __getitem__(self, item):
+        value = self._record.INFO[item]
+        # for some reasons cyvcf2 doesn't split String lists, a known circumstance
+        meta = self._header.infos[item]
+        number, typ = meta["Number"], meta["Type"]
+        if typ == "String" and number == ".":
+            value = value.split(",")
+        return value
+
+    def __setitem__(self, key, value):
+        self._record.INFO[key] = value
+
+    def __contains__(self, item):
+        return item in self._record.INFO
+
+    def get(self, item, default=None):
+        return self._record.INFO.get(item, default)
+
+
 class Cyvcf2VCFWriter(VCFWriter):
     def __init__(self, filename: str, fmt: str, template: VCFReader):
         self._file = Writer(filename, template._file, mode=f"w{fmt}")
-        # self.filename = filename
-        # self._file = pysam.VariantFile(
-        #     self.filename, f"w{fmt}", header=template.header._header
-        # )
-        # self._header = Cyvcf2VCFHeader(self)
 
     def write(self, record: Cyvcf2VCFRecord):
         self._file.write_record(record._record)
-
-
-# class PysamVCFRecord(VCFRecord):
-#     def __init__(self, record: VariantRecord):
-#         self._record = record
