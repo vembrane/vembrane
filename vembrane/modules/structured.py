5263535be35d197aedb33abf9ac2778743add545
Johannes Koester <johannes.koester@uni-due.de>
2025-02-24 11:53:32 +0100

    dbg

--- vembrane/modules/structured.py
+++ vembrane/modules/structured.py
@@ -88,11 +88,11 @@ class CodeHandler(yte.CodeHandler):
 
     def eval(self, expr: str, variables: Dict[str, Any]):
         env = self._env(expr)
-        return eval(env.compiled, variables, env)
+        return eval(expr, variables, env)
 
     def exec(self, source: str, variables: Dict[str, Any]):
         env = self._env(source)
-        return exec(env.compiled, variables, env)
+        return exec(source, variables, env)
 
 
 class ValueHandler(yte.ValueHandler):
